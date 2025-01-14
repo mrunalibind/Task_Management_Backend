db0e4dcf18c99a880d69b55632413712b2d9793f
Mrunali Bind <112799850+mrunalibind@users.noreply.github.com>
2025-01-14 18:32:23 +0530

    Update database link

--- config/database.js
+++ config/database.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 
-let connection = mongoose.connect("mongodb://127.0.0.1:27017/TaskManagementSystem");
-// let connection = mongoose.connect("mongodb+srv://mrunali:mrunalibind@cluster0.tsxywrf.mongodb.net/TaskManagementSystem?retryWrites=true&w=majority");
+let connection = mongoose.connect(process.env.URL);
 
 export default connection;
